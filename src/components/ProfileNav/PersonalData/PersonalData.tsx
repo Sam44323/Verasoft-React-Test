8111a617cf95d3cacf0fb22430df7e818e998dbc
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-01 23:49:40 +0530

    PersonalData components completed

--- src/components/ProfileNav/PersonalData/PersonalData.tsx
+++ src/components/ProfileNav/PersonalData/PersonalData.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faMobile,
+  faBuilding,
+  faHome,
+  faAt,
+} from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./PersonalData.module.css";
 
 const PersonalData: React.FC = () => {
-  return <h1>Personal Data</h1>;
+  return (
+    <div className={styles.personalDataContainer}>
+      <div className={`${styles.userSection} ${styles.commonSectionStyles}`}>
+        <FontAwesomeIcon icon={faUser} color="#B0C6D8" />
+        <p>#12345678</p>
+      </div>
+      <div className={`${styles.numberSection} ${styles.commonSectionStyles}`}>
+        <FontAwesomeIcon icon={faMobile} color="#B0C6D8" />
+        <p>248-555-1000</p>
+      </div>
+      <div
+        className={`${styles.buildingSection} ${styles.commonSectionStyles}`}
+      >
+        <FontAwesomeIcon icon={faBuilding} color="#B0C6D8" />
+        <p>248-555-1000 ext 1023</p>
+      </div>
+      <div className={`${styles.homeSection} ${styles.commonSectionStyles}`}>
+        <FontAwesomeIcon icon={faHome} color="#B0C6D8" />
+        <p>248-555-1000</p>
+      </div>
+      <div className={`${styles.emailSection} ${styles.commonSectionStyles}`}>
+        <FontAwesomeIcon icon={faAt} color="#B0C6D8" />
+        <p>joe.smith@testemail.com</p>
+      </div>
+    </div>
+  );
 };
 
 export default PersonalData;
