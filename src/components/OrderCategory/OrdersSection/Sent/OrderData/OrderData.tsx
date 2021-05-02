c4527b562349b34f1189a62f7230bf32192b6978
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-03 01:30:06 +0530

    styles added to OrderData cmponent

--- src/components/OrderCategory/OrdersSection/Sent/OrderData/OrderData.tsx
+++ src/components/OrderCategory/OrdersSection/Sent/OrderData/OrderData.tsx
@@ -3,7 +3,29 @@ import React from "react";
 import styles from "./OrderData.module.css";
 
 const OrderData: React.FC = () => {
-  return <h1>Order Data</h1>;
+  return (
+    <div className={styles.sentContainer}>
+      <div className={styles.gridFirst}>
+        <div className={styles.detailSection}>
+          <h1>Sat, Apr 14</h1>
+          <p>4:19 PM</p>
+        </div>
+        <div className={styles.detailSection}>
+          <h1>Thank You Bonus</h1>
+          <p>joe.smith@testemail.com</p>
+        </div>
+      </div>
+      <div className={styles.gridSecond}>
+        <div className={styles.detailSection}>
+          <h1>Promotion Email</h1>
+        </div>
+        <div className={styles.detailSection}>
+          <h1>12345546</h1>
+        </div>
+        <button>RESEND</button>
+      </div>
+    </div>
+  );
 };
 
 export default OrderData;
--- src/containers/OrderContainer/OrderContainer.tsx
+++ src/containers/OrderContainer/OrderContainer.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import OrderNav from "../../components/OrderCategory/OrderNav/OrderNav";
 import OrderSectionNav from "../../components/OrderCategory/OrdersSection/OrderSectionNav/OrderSectionNav";
 import OrderColHeadings from "../../components/OrderCategory/OrdersSection/OrderColHeadings/OrderColHeadings";
+import Sent from "../../components/OrderCategory/OrdersSection/Sent/OrderData/OrderData";
 
 const OrderContainer: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const OrderContainer: React.FC = () => {
       <OrderNav />
       <OrderSectionNav />
       <OrderColHeadings />
+      <Sent />
     </>
   );
 };
