528cf13cc60d270e4585ffaaef6513796637e3c7
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-03 00:08:14 +0530

    styling added to the OrderSectionNav component

--- src/components/OrderCategory/OrdersSection/OrderSectionNav/OrderSectionNav.tsx
+++ src/components/OrderCategory/OrdersSection/OrderSectionNav/OrderSectionNav.tsx
@@ -3,7 +3,15 @@ import React from "react";
 import styles from "./OrderSectionNav.module.css";
 
 const OrderSectionNav: React.FC = () => {
-  return <h1>Order Section Nav</h1>;
+  return (
+    <div className={styles.orderSectionNavContainer}>
+      <div className={styles.buttonSection}>
+        <button>SENT</button>
+        <button>ERRORS</button>
+      </div>
+      <h1>RECENT ORDERS</h1>
+    </div>
+  );
 };
 
 export default OrderSectionNav;
--- src/containers/OrderContainer/OrderContainer.tsx
+++ src/containers/OrderContainer/OrderContainer.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import styles from "./OrderContainer.module.css";
 import OrderNav from "../../components/OrderCategory/OrderNav/OrderNav";
+import OrderSectionNav from "../../components/OrderCategory/OrdersSection/OrderSectionNav/OrderSectionNav";
 
 const OrderContainer: React.FC = () => {
   return (
     <>
       <OrderNav />
+      <OrderSectionNav />
     </>
   );
 };
