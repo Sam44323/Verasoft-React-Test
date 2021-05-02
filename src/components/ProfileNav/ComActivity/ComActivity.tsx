c60a4aba8bcebecaf4f52536f0687fc27c24864e
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-02 16:27:15 +0530

    styles added to the ComActivity component

--- src/components/ProfileNav/ComActivity/ComActivity.tsx
+++ src/components/ProfileNav/ComActivity/ComActivity.tsx
@@ -3,7 +3,25 @@ import React from "react";
 import styles from "./ComActivity.module.css";
 
 const ComActivity: React.FC = () => {
-  return <h1>Com Activity</h1>;
+  return (
+    <div className={styles.comContainer}>
+      <h1 className={styles.comHeading}>90-DAY COMMUNICATION ACTIVITY</h1>
+      <div className={styles.comDetails}>
+        <div className={styles.details}>
+          <h1>6</h1>
+          <p>SMS</p>
+        </div>
+        <div className={styles.details}>
+          <h1>4</h1>
+          <p>EMAIL</p>
+        </div>
+        <div className={styles.details}>
+          <h1>1</h1>
+          <p>ORDERS</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ComActivity;
