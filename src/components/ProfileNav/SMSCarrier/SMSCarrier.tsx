5e7bfbb4c2d4829714e68267b33091fcec07f9e0
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-02 17:24:36 +0530

    stylings adde to the SMSCarrier component

--- src/components/ProfileNav/SMSCarrier/SMSCarrier.tsx
+++ src/components/ProfileNav/SMSCarrier/SMSCarrier.tsx
@@ -6,6 +6,10 @@ const SMSCarrier: React.FC = () => {
   return (
     <div className={styles.smsContainer}>
       <h1>SMS CARRIER STATUS</h1>
+      <div className={styles.dataContainer}>
+        <h1>IN</h1>
+        <p>SINCE JUN 3, 2016</p>
+      </div>
     </div>
   );
 };
