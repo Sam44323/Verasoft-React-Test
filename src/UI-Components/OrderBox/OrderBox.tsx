c71f270925d5e1555e0413dc46736f1a4795473c
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-02 21:45:53 +0530

    updated the OrderBox component

--- src/UI-Components/OrderBox/OrderBox.tsx
+++ src/UI-Components/OrderBox/OrderBox.tsx
@@ -3,14 +3,7 @@ import React from "react";
 import styles from "./OrderBox.module.css";
 
 const OrderBox: React.FC = (props) => {
-  return (
-    <button
-      className={styles.buttonSection}
-      onClick={() => console.log(props.children)}
-    >
-      {props.children}
-    </button>
-  );
+  return <button className={styles.buttonSection}>{props.children}</button>;
 };
 
 export default OrderBox;
