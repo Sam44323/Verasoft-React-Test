e616eb632e44b792d467f0ecee77270687558867
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-03 15:29:52 +0530

    added the styles to OrderDaat in Error container

--- src/components/OrderCategory/OrdersSection/Error/OrderData/OrderData.tsx
+++ src/components/OrderCategory/OrdersSection/Error/OrderData/OrderData.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./OrderData.module.css";
 
 const OrderData: React.FC = () => {
-  return <h1>Order Data</h1>;
+  return <div className={styles.errorContainer}></div>;
 };
 
 export default OrderData;
--- src/containers/OrderContainer/OrderContainer.tsx
+++ src/containers/OrderContainer/OrderContainer.tsx
@@ -4,6 +4,7 @@ import OrderNav from "../../components/OrderCategory/OrderNav/OrderNav";
 import OrderSectionNav from "../../components/OrderCategory/OrdersSection/OrderSectionNav/OrderSectionNav";
 import OrderColHeadings from "../../components/OrderCategory/OrdersSection/OrderColHeadings/OrderColHeadings";
 import Sent from "../../components/OrderCategory/OrdersSection/Sent/OrderData/OrderData";
+import Error from "../../components/OrderCategory/OrdersSection/Error/OrderData/OrderData";
 
 const ORDERS = {
   sent: [
