519bc381da9541c7d815f6469bf905c3fc0bf042
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-04 01:54:48 +0530

    basic modal styling added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 
 import UserData from "./containers/UserData/UserData";
 import OrderContainer from "./containers/OrderContainer/OrderContainer";
+import Modal from "./UI-Components/Modal/Modal";
 
 function App() {
   return (
     <>
       <UserData />
       <OrderContainer />
+      {/* <Modal /> */}
     </>
   );
 }
--- src/UI-Components/Modal/Modal.tsx
+++ src/UI-Components/Modal/Modal.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+// will render this modal using creatPortal
 
 import styles from "./Modal.module.css";
 
 const Modal: React.FC = () => {
-  return <h1>Modal</h1>;
+  return (
+    <div className={styles.modalContainer}>
+      <div>
+        <FontAwesomeIcon icon={faTimes} color="#EEEEEE" size="3x" />
+      </div>
+      <h1>Processing</h1>
+    </div>
+  );
 };
 
 export default Modal;
