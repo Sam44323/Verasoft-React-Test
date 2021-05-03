26c805e080bb508d687743876530e124c2e15192
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-03 14:07:39 +0530

    added the props data to OrderData component

--- src/components/OrderCategory/OrdersSection/Sent/OrderData/OrderData.tsx
+++ src/components/OrderCategory/OrdersSection/Sent/OrderData/OrderData.tsx
@@ -2,25 +2,33 @@ import React from "react";
 
 import styles from "./OrderData.module.css";
 
-const OrderData: React.FC = () => {
+const OrderData: React.FC<{
+  id: number;
+  ord: number;
+  sDt: string;
+  sTm: string;
+  title: string;
+  email: string;
+  type: string;
+}> = (props) => {
   return (
     <div className={styles.sentContainer}>
       <div className={styles.gridFirst}>
         <div className={styles.detailSection}>
-          <h1>Sat, Apr 14</h1>
-          <p>4:19 PM</p>
+          <h1>{props.sDt}</h1>
+          <p>{props.sTm}</p>
         </div>
         <div className={styles.detailSection}>
-          <h1>Thank You Bonus</h1>
-          <p>joe.smith@testemail.com</p>
+          <h1>{props.title}</h1>
+          <p>{props.email}</p>
         </div>
       </div>
       <div className={styles.gridSecond}>
         <div className={styles.detailSection}>
-          <h1>Promotion Email</h1>
+          <h1>{props.type}</h1>
         </div>
         <div className={styles.detailSection}>
-          <h1>12345546</h1>
+          <h1>{props.ord}</h1>
         </div>
         <button>RESEND</button>
       </div>
--- src/containers/OrderContainer/OrderContainer.tsx
+++ src/containers/OrderContainer/OrderContainer.tsx
@@ -5,13 +5,51 @@ import OrderSectionNav from "../../components/OrderCategory/OrdersSection/OrderS
 import OrderColHeadings from "../../components/OrderCategory/OrdersSection/OrderColHeadings/OrderColHeadings";
 import Sent from "../../components/OrderCategory/OrdersSection/Sent/OrderData/OrderData";
 
+const ORDERS = {
+  sent: [
+    {
+      id: 1,
+      order_id: 12345546,
+      sent_dt: "2018-04-14",
+      sent_tm: "16:19:00",
+      subject: {
+        title: "ABC Appointment Confirmation",
+        email: "joe.smith@testemail.com",
+      },
+      type: "Gift Card Email",
+    },
+    {
+      id: 2,
+      order_id: 12345546,
+      sent_dt: "2018-04-14",
+      sent_tm: "16:19:00",
+      subject: {
+        title: "Receipt #12345546",
+        email: "joe.smith@testemail.com",
+      },
+      type: "Order Email Receipt",
+    },
+  ],
+};
+
 const OrderContainer: React.FC = () => {
   return (
     <>
       <OrderNav />
       <OrderSectionNav />
       <OrderColHeadings />
-      <Sent />
+      {ORDERS.sent.map((item) => (
+        <Sent
+          key={item.id}
+          id={item.id}
+          ord={item.order_id}
+          sDt={item.sent_dt}
+          sTm={item.sent_tm}
+          title={item.subject.title}
+          email={item.subject.email}
+          type={item.type}
+        />
+      ))}
     </>
   );
 };
